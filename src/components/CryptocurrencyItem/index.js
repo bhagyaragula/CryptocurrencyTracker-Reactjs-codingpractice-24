b72024da8e76498ab8c4f6dd73ca42fb58db3012
Bhagya <127249618+bhagyaragula@users.noreply.github.com>
2025-03-11 11:08:26 +0530

    Update index.js

--- src/components/CryptocurrencyItem/index.js
+++ src/components/CryptocurrencyItem/index.js
@@ -2,21 +2,22 @@
 import './index.css'
 
 const CryptocurrencyItem = props => {
-  const {cryptoCurrencyData} = props
+  const {cryptoCurrenciesData} = props
+  const { currencyLogoUrl, currencyName, usdValue, euroValue, } = cryptoCurrenciesData 
 
   return (
     <li className="item-container">
       <div className="logo-container">
         <img
           className="logo-img"
-          src={cryptoCurrencyData.currencyLogoUrl}
-          alt={cryptoCurrencyData.currencyName}
+          src={currencyLogoUrl}
+          alt={currencyName}
         />
-        <p className="currency-name">{cryptoCurrencyData.currencyName}</p>
+        <p className="currency-name">{currencyName}</p>
       </div>
       <div className="usd-euro-container">
-        <p className="currency-value">{cryptoCurrencyData.usdValue}</p>
-        <p className="currency-value">{cryptoCurrencyData.ueroValue}</p>
+        <p className="currency-value">{usdValue}</p>
+        <p className="currency-value">{ueroValue}</p>
       </div>
     </li>
   )
